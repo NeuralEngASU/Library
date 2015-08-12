f006b7d9f65a99a31e27e33c9214d9c7213d50ea
Kevin O'Neill <kjon.oneill@gmail.com>
2015-08-12 15:45:49 -0700

    Added TDT Data extraction and SyncSignal decode
    Added TDT Data extraction and SyncSignal decode
    -Kevin

--- koneill/Utilities/SyncSignalDecode.m
+++ koneill/Utilities/SyncSignalDecode.m
@@ -1,24 +1,18 @@
 %% Sync Signal Decode
 
-%% EDF Read
-filePath = 'D:\Data\HumanECoG\2015PP01\2015PP01_D02B_Sz1.edf';
-
-[Header, data] = edfread(filePath);
-
-syncData = data(1,:);
-%%
-
-Fs = ceil(Header.samples(1)/Header.duration);
-
-MaxSyncTSLength = 4.3*Fs; % Units samples. Timestamps in sync signal are 43 bits long and sent at a rate of 10 Hz.
-MinSyncTSPeriod = 10*Fs; % Units samples. Timestamps in sync signal can have a minimum period of 10 sec.
-BitLength = 0.1*Fs; % Units samples.
+function timeMat = SyncSignalDecode(syncData, Fs)
+% Function setup
+maxSyncTSLength = floor(4.3*Fs); % Units samples. Timestamps in sync signal are 43 bits long and sent at a rate of 10 Hz.
+minSyncTSPeriod = floor(10*Fs); % Units samples. Timestamps in sync signal can have a minimum period of 10 sec.
+% BitLength = floor(0.1*Fs); % Units samples.
+bitLength = floor(maxSyncTSLength/43);
 
 % Finding voltages (Values) for each timestamp (TS) in the SyncSignal
 params.Fs = Fs;
 params.eventGap = 3; % [seconds] Gap threshold between event groups
 eventIdx = FindStepEvents(syncData, params);
-%%
+
+% Compute timestamp
 strMat = [];
 % loop over each event
 for ii = 1:size(eventIdx,2)-1
@@ -27,11 +21,7 @@ for ii = 1:size(eventIdx,2)-1
     if size(testEvent,2) <= 2
         timeStamp{ii} = nan;
     else
-        
-        signal = zeros(1, MaxSyncTSLength);
-        % Calculate bit length
-        bitLen = diff(testEvent(1:2))/10;
-        
+        signal = zeros(1, maxSyncTSLength);
         for jj = 1:2:size(testEvent,2)
             if jj+1>size(testEvent,2)
                 signal(testEvent(jj):end) = 1;
@@ -40,12 +30,12 @@ for ii = 1:size(eventIdx,2)-1
             end
         end
         
-        signal2 = reshape(signal, 50, 43);
+        signal2 = reshape(signal(1:bitLength*43), bitLength, 43);
         syncBits = round(mean(signal2,1));
-        if syncBits(end) == 0
-            disp(ii)
-            syncBits = ~syncBits;
-        end
+%         if syncBits(end) == 0
+%             disp(ii)
+%             syncBits = ~syncBits;
+%         end
         syncBitsStr = num2str(fliplr(syncBits(:,12:end))')';
         syncBitsStr = syncBitsStr(1:3:size(syncBitsStr,1),:);
         syncSec = bin2dec(syncBitsStr);
@@ -53,59 +43,27 @@ for ii = 1:size(eventIdx,2)-1
         strMat = [strMat;syncBitsStr];
         
         % Convert from Labview to Matlab time (64800 represents seconds in 18 hours)
-Labview2MatlabOffset = etime(datevec('01-01-1904 00:00:00','mm-dd-yyyy HH:MM:SS'),datevec('01-01-0000 00:00:00','mm-dd-yyyy HH:MM:SS')) + 64800;
-% SyncTSDays = (SyncTSSec + Labview2MatlabOffset)/86400;
-
+        labview2MatlabOffset = etime(datevec('01-01-1904 00:00:00','mm-dd-yyyy HH:MM:SS'),datevec('01-01-0000 00:00:00','mm-dd-yyyy HH:MM:SS')) + 64800;
 
         labViewTime = datenum('19040101 00:00:00', 'yyyymmdd HH:MM:SS');
-        expTime = (Labview2MatlabOffset + syncSec)/8.64e4;
+        expTime = (labview2MatlabOffset + syncSec)/8.64e4;
         matlabTime = datenum('0000101 00:00:00', 'yyyymmdd HH:MM:SS');
         offset = labViewTime - matlabTime;
         
-        SyncTSDateVec(ii,:) = datevec(expTime);
+%         SyncTSDateVec(ii,:) = datevec(expTime);
         
-%         expTime = offset + syncSec/8.64e4;
-%         matlabTime = datenum('0000', 'yyyy');
-%         timeRef = datenum('1970', 'yyyy'); % Setup a reference date (Jan-1 1970)
-%         startTimeMatlab = timeRef + tsqStartTimeStamp / 8.64e4; % Convert the tsqStartTimeStamp into days and add the days to the reference date
-%         startTimeMatlab = startTimeMatlab - 7/24; % Arizona time is GMT-7:00
-%         startTimeMatlabString = datestr(startTimeMatlab, 'yyyymmdd HH:MM:SS'); % Convert to string
-
         timeMat(ii) = expTime;
 
         timeStamp{ii} = expTime;
     end   
 end % END FOR
 
-for kk = 1:size(timeStamp,2)
-    if ~isnan(timeStamp{kk})
-        disp(datestr(timeStamp{kk}, 'yyyymmdd HH:MM:SS'))
-    end
-end
+% for kk = 1:size(timeStamp,2)
+%     if ~isnan(timeStamp{kk})
+%         disp(datestr(timeStamp{kk}, 'yyyymmdd HH:MM:SS'))
+%     end
+% end % END FOR
 
-%%
+end % END FUNCTION SyncSignalDecode
 
-[SyncIdxs, SyncMS] = PulseCounter(SyncSignal,Fs,10000);
-SyncTSStartIdxs = SyncIdxs([1,find(diff(SyncIdxs)>MaxSyncTSLength)+1])';
-SyncTSStartIdxsMat = repmat(SyncTSStartIdxs,1,MaxSyncTSLength) + repmat(0:MaxSyncTSLength-1,size(SyncTSStartIdxs,1),1);
-SyncTSValues = SyncSignal(SyncTSStartIdxsMat);
-
-% Finding the voltage values at the middle of each 200 sample bit (43 total)
-SyncTSBits = SyncTSValues(:,100:BitLength:MaxSyncTSLength-100)>10000;
-
-% Converting to decimal
-SyncTSBitsStr = num2str(fliplr(SyncTSBits(:,12:end))')';
-SyncTSBitsStr = SyncTSBitsStr(1:3:size(SyncTSBitsStr,1),:);
-SyncTSSec = bin2dec(SyncTSBitsStr);
-
-% Convert from Labview to Matlab time (64800 represents seconds in 18 hours)
-Labview2MatlabOffset = etime(datevec('01-01-1904 00:00:00','mm-dd-yyyy HH:MM:SS'),datevec('01-01-0000 00:00:00','mm-dd-yyyy HH:MM:SS')) + 64800;
-SyncTSDays = (SyncTSSec + Labview2MatlabOffset)/86400;
-SyncTSDateVec = datevec(SyncTSDays);
-
-% Saving to Structure
-for k=1:size(SyncTSDateVec,1)    
-    SyncStruct(k).Date = SyncTSDateVec(k,:);
-    SyncStruct(k).Samples = SyncTSStartIdxs(k);
-    SyncStruct(k).Seconds = SyncTSStartIdxs(k)/Fs;            
-end
+% EOF
