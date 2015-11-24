5ed87088589659c5bbced46cad3196c3f8948f94
kashmont <karich@asu.edu>
2015-11-24 15:54:50 -0700

    Fixed Merge 2
    Conflicts:
    	kashmont/EMD/siftstop.m

--- kashmont/EMD/siftstop.m
+++ kashmont/EMD/siftstop.m
@@ -10,12 +10,8 @@ minpts = (find(diff(rise_fall)>0))+1;
 s = length(maxpts)+length(minpts);
 
 % stopsift = s<4;
-<<<<<<< HEAD
-
 f=2*(round((length(x))/Fs));
-=======
-f=(round((length(x))/Fs)*2);
->>>>>>> origin/master
+
 
 extdiff=length(maxpts)-length(minpts);
 
@@ -26,15 +22,14 @@ else
     ppdiff=x(maxpts)-x(minpts(1:length(maxpts)));
     avgdiff=mean(ppdiff);
 end
-<<<<<<< HEAD
-    
-stopsift = s<f| avgdiff<(2/1000000);
-%s<f
-=======
 
+% Micro
+% stopsift = s<f| avgdiff<(2/1000000);
+
+% Macro
 stopsift = s<f | avgdiff<20;
 
->>>>>>> origin/master
+
 end
 
 
